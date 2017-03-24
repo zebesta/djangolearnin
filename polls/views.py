849f0dfcfe13a70f5c9f9e5554054db31d839ed5
zebesta <chris.sebesta@gmail.com>
2017-03-24 17:22:46 -0400

    added some really simple rendered html templates

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world! Polls index")
+    latest_questions_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_questions_list': latest_questions_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
