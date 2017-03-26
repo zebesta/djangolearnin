aa41fd157d8e3618d9c46b615cb931b186983219
zebesta <chris.sebesta@gmail.com>
2017-03-26 17:50:27 -0400

    added details view and rendered it

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
-# from django.template import loader
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from .models import Question
@@ -7,15 +6,17 @@ from .models import Question
 # Create your views here.
 def index(request):
     latest_questions_list = Question.objects.order_by('-pub_date')[:5]
-    # template = loader.get_template('polls/index.html')
     context = {
         'latest_questions_list': latest_questions_list,
     }
-    # return HttpResponse(template.render(context, request))
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s"
