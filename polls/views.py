4bced1dbcdb47dfacfb75b811ea84a88eddc9959
zebesta <chris.sebesta@gmail.com>
2017-03-26 19:00:53 -0400

    handling vote on the server

--- polls/views.py
+++ polls/views.py
@@ -20,4 +20,16 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # on error redisplay the form again
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.cotes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
